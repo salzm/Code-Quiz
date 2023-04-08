2b34c4e843ebe50c418a122288d7263481b99ab6
salzm <salmonzoor@gmail.com>
2023-04-08 12:14:22 -0400

    made structure to html,css and javascript files

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+// First made a "dictionary" which holds the value of questions. 
+
+const questions = {
+    question1: "What does HTML Stand for?",
+  };
+
+  document.getElementById("Question1").innerHTML = questions.question1; // added our question and then used getElmentbyID to refrence the class in HTML file to assign the value of question1
+
