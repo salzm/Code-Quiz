9936bdc2a659a614f7acf4d589ce6dabf83c6d8e
salzm <salmonzoor@gmail.com>
2023-04-11 16:54:01 -0400

    Updated code. Can view and click on questions and show score

--- Assets/script.js
+++ Assets/script.js
@@ -1,80 +1,165 @@
+// Timer information. A constant starting minutes of 10 min. The mintues is tomes 60 for 60 sections by mintues.
+
+// Then we get element by id countdown from the HTML ID
+
+const startMintues= 1;
+let time = startMintues * 60;
+const countdownEL = document.getElementById('countdown');
+
+setInterval(updateCountDown, 1000);
+
+// making a funtion to countdown the ID
+
+function updateCountDown(){
+    const minutes = Math.floor(time / 60); //counting minutes. All the seconds and divide 60 to get the minutes and we use math.floor to retreive lowest number without decimels
+    let seconds = time % 60;
+
+    seconds = seconds <10 ? '0' + seconds : seconds; // keeps the seconds counting.
+   
+    countdownEL.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
+
+
+
 
 // First made a "dictionary" which holds the value of questions. Total 3 Questions
 
 const questions = [
-  {
-
+{
     question: "What does HTML Stand for?",
     answers: [
-      { text: "Hypertext Markup Language", correct: true},
-      { text: "Hyperspeed Markup Language", correct: false},
-      { text: "Hypertext Marking Language", correct: false},
-      { text: "N/A", correct: false},
+     { text: "HyperText Markup Language", correct : true},
+     { text: "HyperText Markups Language", correct : false},
+     { text: "HyperText Marking Language", correct : false},
+     { text: "HyperText Mass Language", correct : false},
+
 
     ]
-  },
-  {
-    question: "What does CSS Stand for?",
+},
+
+{
+  question: "What does CSS Stand for?",
     answers: [
-      { text: "Casanova Style Sheet", correct: false},
-      { text: "Cascadding Markup Language", correct: false},
-      { text: "Cascadding Style Sheet", correct: true},
-      { text: "Castle Style Sheet", correct: false},
-
+     { text: "Cascading Style S", correct : false},
+     { text: "HyperText Markups Language", correct : false},
+     { text: "Cascading Style Sheets", correct : true},
+     { text: "Cascading Stylisitc Sheets", correct: false},
     ]
-  },
-  {
-    question: "What is WebAPI?",
+},
+
+{
+  question: "What does JS Stand for?",
     answers: [
-      { text: "Something you can Google", correct: false},
-      { text: "Something on the web in your browser", correct: false},
-      { text: "API of Websites", correct: false},
-      { text: "an application programming interface for either a web server or a web browser.", correct: true},
+     { text: "Java Scripting", correct : false},
+     { text: "Java Science", correct : false},
+     { text: "Javascript", correct : true},
+     { text: "Java Scripts", correct : false}, 
+
 
     ]
-  }
+}
+
 ];
+// Adding these 3 html elements to JS
 
-const questionElement= document.getElementById("question"); // Got elements from HTML for the ID of question
-const answerButtons= document.getElementById("answer-buttons"); // Got elements from HTML for the ID of answsers
-const nextbutton= document.getElementById("next-btn"); // Got elements from HTML for the ID of question
+const questionElement = document.getElementById("question");
+const  answerButtons = document.getElementById("answer-buttons");
+const  nextButton = document.getElementById("next-btn");
+
+// Variables to store question and answer index
+
 
 let currentQuestionIndex = 0;
-let score = 0;
+let score =0;
 
-function starQuiz() {    // created a funtiont to make the starting index 0 for the questions so when clicking on next button it will move on the next set of questions and answers
-  currentQuestionIndex=0; // WIll reset the questions and score to 0
-  score = 0;
-  nextbutton.innerHTML = "Next"; //replay button 
-  showQuestion(); //Will display the questions
+function startQuiz() {
+  currentQuestionIndex =0;
+  score =0;
+  nextButton.innerHTML = "Next";
+  showQuestion();  // Will display questions
 }
 
-function showQuestion() {    // Created a funcation for show question where the first index will be question 1. Index will be 0
-  resetState(); //rest previous question
+function showQuestion() {
+  resetState(); //This will reset previous question and answers
   let currentQuestion = questions[currentQuestionIndex];
-  let questionNo = currentQuestionIndex + 1; // increment Number 1 Number 2 etc
-  questionElement.innerHTML = questionNo + ". " + currentQuestion.question; // Adding element to HTML
+  let questionNumber = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNumber + ". " + currentQuestion.question;
+
+currentQuestion.answers.forEach(answer => {
+  const button = document.createElement("button");
+  button.innerHTML = answer.text; //calling the answer and text from the first block of code in JS
+  button.classList.add("btn");
+  answerButtons.appendChild(button); //display the button inside the div
+  
+  if(answer.correct){ // If answer is correct after selecting answer run this loop.
+    button.dataset.correct = answer.correct;
+  }
+  button.addEventListener("click", selectAnswer); //selectAnswer is a function.
 
 
-  currentQuestion.answers.forEach(answer => {                       // Display the answers
-      const button = document.createElement("button"); // it will go to the answers and get and display the text in button. Create a button tag called showButton
-      button.innerHTML = answer.text;
-     button.classList.add("btn"); // Need to add a class name called btn in the button. 
-     answerButtons.appendChild(button); // It needs to be displayed in the div buttons so we need to append the child to the parent ID
+});
 
-  });   
-} 
-
+}
 function resetState() {
-  nextButton.style.display = "none";
-  while(answerButtons.firstChild){
-    answerButtons.removeChild(answerButtons.firstChild);
+  nextButton.style.display ="none";
+  while(answerButtons.firstChild) {
+      answerButtons.removeChild(answerButtons.firstChild); // remove the previous child elements
+  }
+}
+
+function selectAnswer(e) { //Defining Function
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true"; // Function is saying if correct then it is true
+
+  if(isCorrect) { // If else statement saying that if the selected button is correct print correct, else print incorrect.
+    selectedBtn.classList.add("correct");
+    score ++; // This will increase score by 1 every time you get an answer right
+  }  else{
+    selectedBtn.classList.add("incorrect");
   }
 
+
+    Array.from(answerButtons.children).forEach(button => { // This shows if the button or answer turns green, let the system know it is correct, show the hiddent next button to move forward to the next question.
+      // otherwise if wrong, show the correct answer.
+        if(button.dataset.correct === "true") {
+          button.classList.add ("correct");
+        }
+
+        button.disabled = true;
+    });
+
+    nextButton.style.display ="block"; //will show the next question
+};
+
+function showScore() { // show score at the end of the game!
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Try Again?"
+  nextButton.style.display = "block";
+};
+
+function handleTheNextButton() {
+  currentQuestionIndex++ //increase question index by 1 when we click on next button
+  if(currentQuestionIndex < questions.length){
+    showQuestion();
+  } else{
+    showScore(); //define score function
+  }
 }
- 
-starQuiz(); // calling the function
- 
+
+
+
+nextButton.addEventListener("click", () => {
+if(currentQuestionIndex <questions.length) {
+    handleTheNextButton(); //empty function need to define to move to the next question
+} else{
+  startQuiz();
+}
+} );
+
+
+startQuiz();  // Call function
 
 
 
@@ -97,27 +182,41 @@ starQuiz(); // calling the function
 
 
 
-//   Timer information. A constant starting minutes of 10 min. The mintues is tomes 60 for 60 sections by mintues.
 
-// Then we get element by id countdown from the HTML ID
 
-// const startMintues= 1;
-// let time = startMintues * 60;
-// const countdownEL = document.getElementById('countdown');
 
-// setInterval(updateCountDown, 1000);
 
-// // making a funtion to countdown the ID
 
-// function updateCountDown(){
-//     const minutes = Math.floor(time / 60); //counting minutes. All the seconds and divide 60 to get the minutes and we use math.floor to retreive lowest number without decimels
-//     let seconds = time % 60;
 
-//     seconds = seconds <10 ? '0' + seconds : seconds; // keeps the seconds counting.
-   
-//     countdownEL.innerHTML = `${minutes}: ${seconds}`;
-//     time--;
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
